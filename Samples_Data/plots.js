31c64540427c1c19115ae4bc83b7905bd545affc
Aarani Sivasekaram <aarani.sivasekaram@gmail.com>
2021-03-20 19:57:26 -0400

    Added samples data and web files

---
+++ Samples_Data/plots.js
@@ -0,0 +1,33 @@
+// Read in samples file
+d3.json("samples.json").then(function(data){
+    console.log(data);
+});
+
+// // Retrieve weekly frequency for each volunteer
+// d3.json("samples.json").then(function(data){
+//     wfreq = data.metadata.map(person => person.wfreq);
+//     console.log(wfreq);
+// });
+
+// // Retrive weekly frequency for each volunteer and sort by descending order
+// d3.json("samples.json").then(function(data){
+//     wfreq = data.metadata.map(person =>
+// person.wfreq).sort((a,b) => b - a);
+//     console.log(wfreq);
+// });
+
+// Retrive weekly frequency for each volunteer and sort by descending order and delete null values
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person =>
+person.wfreq).sort((a,b) => b - a);
+    filteredWfreq = wfreq.filter(element => element !=
+null);
+    console.log(filteredWfreq);
+});
+
+// Display metadata of any individual from samples dataset
+d3.json("samples.json").then(function(data){
+    firstPerson = data.metadata[0];
+    Object.entries(firstPerson).forEach(([key, value]) =>
+      {console.log(key + ': ' + value);});
+});
