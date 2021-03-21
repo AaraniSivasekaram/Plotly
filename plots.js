b12ecdf1460bc6bcbf95f43e6386e6015695bcfb
Aarani Sivasekaram <aarani.sivasekaram@gmail.com>
2021-03-20 22:56:25 -0400

    Updated html and plots file to display two datasets

--- plots.js
+++ plots.js
@@ -1,48 +1,31 @@
-// Plotly.newPlot("plotArea", [{x: [1, 2, 3,4,5,6,7,8], y: [10, 20, 30, 40, 30, 20,10, 40]}]);
-
-// Create bar chart
-// var trace = {
-//     x: ["burrito", "pizza", "chicken"],
-//     y: [10, 18, 5],
-//     type: "bar"
-//  };
-
-// var layout = {
-//     title: "Luncheon Survey",
-//     xaxis: {title: "Food Option"},
-//     yaxis: {title: "Number of Respondents"}
-// };
-
-//  Plotly.newPlot("plotArea", [trace], layout);
-
-// Drinks bar chart
-// var trace = {
-//     labels: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita", "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
-//     values: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-//     type: "pie"
-//     };
-
-// var data = [trace];
-
-// var layout = {
-//     title: "Pie Chart of Drinks",
-// };
-
-//     Plotly.newPlot("plotArea", data, layout);
-
-// Scatterplot
-var trace = {
-    x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita", "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
-    y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-    mode: "markers",
-    type: "scatter",
-    marker: { size: 12}
+function init() {
+    data = [{
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 4, 8, 16] }];
+    Plotly.newPlot("plot", data);
+  };
+  
+  d3.selectAll("#dropdownMenu").on("change", updatePlotly);
+  function updatePlotly() {
+    var dropdownMenu = d3.select("#dropdownMenu");
+    var dataset = dropdownMenu.property("value");
+  
+    var xData = [1, 2, 3, 4, 5];
+    var yData = [];
+  
+    if (dataset === 'dataset1') {
+      yData = [1, 2, 4, 8, 16];
     };
-
-var data = [trace];
-
-var layout = {
-    title: "Drinks Preferences",
-};
-
-    Plotly.newPlot("plotArea", data, layout);
+  
+    if (dataset === 'dataset2') {
+      yData = [1, 10, 100, 1000, 10000];
+    };
+  
+    var trace = {
+      x: [xData],
+      y: [yData],
+    };
+    Plotly.restyle("plot", trace);
+  };
+  
+  init();
